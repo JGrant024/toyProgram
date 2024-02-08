90a8e67028c117faf2783d0280321e3b0618a1d0
JGrant024 <jonathan.grant23@icloud.com>
2024-02-07 23:03:09 -0500

    added fetch request to pull login info from Github updated input

--- js/main.js
+++ js/main.js
@@ -5,21 +5,50 @@ document.addEventListener("DOMContentLoaded", () => {
   const githubForm = document.createElement("form");
   githubForm.id = "githubForm";
 
-  const userNameLabel = document.createElement("label");
-  userNameLabel.textContent = "Username";
-
   const usernameInput = document.createElement("input");
   usernameInput.type = "text";
   usernameInput.id = "username";
   usernameInput.name = "username";
+  usernameInput.placeholder = "Github Username ";
+
+  function fetchUserData() {
+    fetch("https://api.github.com/users/JGRANT024")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // Set the value of the username input field to the retrieved username
+      })
+      .catch((error) => {
+        console.log("Error fetching user data:", error);
+      });
+  }
+
+  //  creates label for username
+  const userNameLabel = document.createElement("label");
+  userNameLabel.textContent = "Username";
 
   const submitButton = document.createElement("button");
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
   submitButton.type = "submit";
   submitButton.textContent = "Submit";
-  console.log(submitButton);
 
+  // Handle form submission
+  githubForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const username = usernameInput.value;
+
+    console.log("Submitted username:", username);
+    // You can add further processing or submission logic here
+  });
+
+  // adds items to username and input
   githubForm.appendChild(userNameLabel);
   githubForm.appendChild(usernameInput);
+  githubForm.appendChild(submitButton);
 
+  // adds form to DOM
   document.body.appendChild(githubForm);
+  fetchUserData();
 });
