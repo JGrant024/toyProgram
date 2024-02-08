f19c86ad14b16d2487c3aa42b0d21112cb96bce0
JGrant024 <jonathan.grant23@icloud.com>
2024-02-07 21:13:44 -0500

    added read me file with directions updated index.html file added username and input in js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("Whats up! ")
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM Ready!");
+
+  // create form for Github username
+  const githubForm = document.createElement("form");
+  githubForm.id = "githubForm";
+
+  const userNameLabel = document.createElement("label");
+  userNameLabel.textContent = "Username";
+
+  const usernameInput = document.createElement("input");
+  usernameInput.type = "text";
+  usernameInput.id = "username";
+  usernameInput.name = "username";
+
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.textContent = "Submit";
+  console.log(submitButton);
+
+  githubForm.appendChild(userNameLabel);
+  githubForm.appendChild(usernameInput);
+
+  document.body.appendChild(githubForm);
+});
